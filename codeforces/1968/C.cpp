74d6dd98c84b92157dca1b65786fb1d21224e744
Mohabbat <mohabbat.bd2020@gmail.com>
2024-05-05 20:46:00 +0600

    Add solution for problem `C - Assembly via Remainders` Link: https://codeforces.com/contest/1968/problem/C Tags: constructive algorithms, number theory Ref: https://codeforces.com/contest/1968/submission/259647375

---
+++ codeforces/1968/C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+const int N=1e9;
+
+
+
+void solve(){
+    ll len; cin >> len;
+    vector<ll> v(len-1);
+    for(int x=0; x<len-1; x++) cin >> v[x];
+
+
+    vector<ll> ans(len);
+    ll mx=N;
+    ans[len-1] = v[len-2];
+    ans[len-2] = mx;
+
+    for(int x=len-3; x>=0; x--){
+        mx-=v[x];
+        ans[x]=mx;
+    }
+
+    for(auto val: ans) cout << val << " ";
+    cout << endl;
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
