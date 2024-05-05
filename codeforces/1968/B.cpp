ffb06024bdde65eb4f3b5bb4f9470bf8ba09e309
Mohabbat <mohabbat.bd2020@gmail.com>
2024-05-05 20:11:00 +0600

    Add solution for problem `B - Prefiquence` Link: https://codeforces.com/contest/1968/problem/B Tags: greedy, two pointers Ref: https://codeforces.com/contest/1968/submission/259643405

---
+++ codeforces/1968/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    ll len1, len2; cin >> len1 >> len2;
+    string a,b; cin >> a >> b;
+
+    ll l=0,r=0;
+    ll ans=0;
+    while(l<len1 && r<len2){
+        if(a[l]==b[r]){
+            ans++;
+            l++;
+            r++;
+        }
+        else r++;
+    }
+
+    cout << ans << endl;
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
