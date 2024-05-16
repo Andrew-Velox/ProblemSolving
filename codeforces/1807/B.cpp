2210a9ac17d4a2c1569e98e40a47d74a11a84a02
Mohabbat <mohabbat.bd2020@gmail.com>
2024-05-16 23:14:00 +0600

    Add solution for problem `B - Grab the Candies` Link: https://codeforces.com/contest/1807/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1807/submission/261232530

---
+++ codeforces/1807/B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    ll len; cin>> len;
+    
+    ll cnt_odd=0,even=0;
+    for(int x=0; x<len; x++){
+        ll val; cin >> val;
+        if(val%2==0) cnt_odd+=val;
+        else even+=val;
+    }
+
+    if(cnt_odd>even) YES;
+    else NO;
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
