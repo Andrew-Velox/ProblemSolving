869ca134f2fe5ecef078ea65cd28e4e14d7f6b29
Mohabbat <mohabbat.bd2020@gmail.com>
2024-06-11 20:37:00 +0600

    Add solution for problem `A - Creating Words` Link: https://codeforces.com/contest/1985/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1985/submission/265218432

---
+++ codeforces/1985/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define gcd(x,y) __gcd(x,y)
+#define lcm(x,y) ((x/gcd(x,y))*y)
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    string a,b; cin >> a >> b;
+
+    cout << b[0];
+    for(int x=1; x<3; x++) cout << a[x];
+    cout << " ";
+
+    cout << a[0];
+    for(int x=1; x<3; x++) cout << b[x];
+    cout << endl;
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
