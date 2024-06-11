bdd39957c658970bd54e4a50897fecc9dc1bfb49
Mohabbat <mohabbat.bd2020@gmail.com>
2024-06-11 20:50:00 +0600

    Add solution for problem `B - Maximum Multiple Sum` Link: https://codeforces.com/contest/1985/problem/B Tags: brute force, math, number theory, *800 Ref: https://codeforces.com/contest/1985/submission/265256473

---
+++ codeforces/1985/B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef velox
+#include "debug.h"
+#else
+#define debug(x...)
+#endif
+
+#define ll long long
+#define srt(v) sort(v.begin(),v.end())
+#define grtsrt(v) sort(v.begin(),v.end(),greater<ll>())
+#define unq(v) v.erase(unique(v.begin(),v.end()),v.end())
+#define rev(v) reverse(v.begin(),v.end())
+#define gcd(x,y) __gcd(x,y)
+#define lcm(x,y) ((x/gcd(x,y))*y)
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define endl '\n'
+#define fx(x) fixed<<setprecision(x)
+
+void error_txt(){
+    #ifdef velox
+    freopen("error.txt","w",stderr);
+    #endif
+}
+
+
+
+void solve(){
+    ll n; cin >> n;
+
+
+    ll ans=INT_MIN;
+
+
+    ll in=1;
+    for(int x=2; x<=n; x++){
+        ll sum=0;
+        for(int y=1; y*x<=n; y++){
+            sum+=x*y;
+        }
+        if(sum>=ans){
+            ans=sum;
+            in=x;
+        }
+    }
+
+
+    cout << in << endl;
+    
+}
+
+int main(){
+    // error_txt();
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll t; cin >> t;
+    while(t--) solve();
+    return 0;
+}
